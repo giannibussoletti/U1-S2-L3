6bc749774e62903dc85ea852f28ab81af8b49135
Gianni <BussolettiGianni@gmail.com>
2026-03-11 10:04:50 +0100

    Update recap.js

--- recap/recap.js
+++ recap/recap.js
@@ -45,7 +45,7 @@ const objects = [
 ]
 // accesso agli elementi
 // l'unico fattore discriminante tra gli elementi di un qualsiasi array è la loro POSIZIONE nell'array
-// ogni array ha un primo elemento, un ultimo elemento e un numero variabile elementi intermedi
+// ogni array ha un primo elemento, un ultimo elemento e un numero variabile e potenzialmente infinito elementi intermedi
 // quindi è proprio con la POSIZIONE che noi ACCEDIAMO agli elementi di un qualsiasi array!
 // tramite la notazione -> nomeArray[posizioneElemento]
 // le posizioni nell'ARRAY partono sempre dalla posizione (INDICE o INDEX) ZERO -> 0
@@ -66,3 +66,55 @@ stringhe.pop() // rimuove l'ultimo elemento in coda
 stringhe.unshift("nuovo primo elemento") // aggiunge un oggetto in cima (all'inizio) dell'ARRAY
 stringhe.shift() // rimuove dalla cima l'oggettto
 stringhe.splice(2, 1) // tolgo "tra noi" perché ho acceduto alla seconda posizione nell'indice e gli ho detto di togliere solo 1 elemento
+
+// LOGICA CONDIZIONALE
+
+// È normalissimo che i nostri ragionamenti tradotti in codice NON sempre procedano in linea retta;
+// alle volte è necessario che il codice prenda una decisione a partire da determinate condizioni.
+// es. se hai preso almeno 18 all'esame, l'hai superato
+const myVoto = 17
+
+// ESEMPIO
+// SE hai preso almeno 18, stampa il primo console.log()
+// ALTRIMENTI stampa il secondo
+if (myVoto >= 18) {
+  console.log("Congratualzioni hai superato l'esame")
+} else {
+  console.log("Peccato non ci sei riuscito")
+}
+
+// è possibile avere anche MULTIPLE  condizioni da verificare
+const myName = "Gianni"
+
+if (myName === "Gianni") {
+  console.log("Benvenuto, Gianni!")
+} else if (myName == "Chistian") {
+  console.log("Benvenuto, Christian!")
+} else if (myName == "Franco") {
+  console.log("Bentornato, Franco!") // Ci possono essere potenzialmente infiniti else if, ma ci sono soluzioni più pratiche
+} else {
+  console.log("Benvenuto, Sconosciuto!")
+}
+
+const nomeApprovato = myName === "Stefano" // nomeApprovato vale true
+
+if (nomeApprovato) {
+  // perchè nomeApprovato è già true
+  console.log("Nome approvato!")
+}
+
+const anotherName = "Davide"
+
+if (anotherName) {
+  // questo messaggio viene stampato!
+  console.log("Ciao!")
+}
+
+// il messaggio viene stampato con if('Davide') perchè in JS tutti i possibili valore, alla fine della fiera,
+// possono venire "castati" in un tipo booleano (TRUE o FALSE)
+// questo è il significato delle parole "TRUTHY" e "FALSY" in JS.
+// in JS TUTTI i valori sono TRUTHY (tendono a true), a parte questi:
+// "" una stringa vuota
+// 0 o -0
+// false
+// null/undefined/Nan valori che non riportato un significato specifico
