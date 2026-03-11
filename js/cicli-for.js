40333fe5585e34c1d715fcc586bec87c476f9296
Gianni <BussolettiGianni@gmail.com>
2026-03-11 12:02:02 +0100

    for files

--- js/cicli-for.js
+++ js/cicli-for.js
@@ -7,3 +7,24 @@
 // esistono principalmente due tipi di cicli
 // a) CICLI che NON conoscono a priori il numero di iterazioni necessario per finire l'operazione (es. while)
 // b) CICLI che CONOSCONO a priori il numero di iterazioni per completare il task (es. for)
+
+// CICLO FOR
+// Un ciclo for è un blocco di codice che viene ri-eseguito un numero PRE-FISSATO di volte!
+// Il numero di volte lo decido io e dipende dal numero di iterazioni desiderate.
+
+//for(creazioneContatore; verificaContatore; incrementoContatore)
+// for crea una variabile volante, e il suo nome di solito è identificato con la lettera "i" per index
+for (
+  let i = 0;
+  i < 5;
+  i++ // i++ è una versione contratta di i = i + 1
+) {
+  console.log("il codice è stato ripetuto: ", i + 1)
+  // sezione per il codice da ripetere
+}
+// 1) si crea il contatore i che solitamente parte da 0
+// 2) il ciclo for verifica che i sia minore di es. 5, se questa comparazione torna TRUE si entra nel for
+// 3) al raggiungimento della graffa finale, i viene incrementato di 1 ("i++")
+// 4) viene riferificato se i è ancora più piccolo di 5, se torna TRUE si ri-esegue il for
+// 5) ...
+// 6) quando i era 4 e viene incrementata a 5, la condizione i < 5 tornerà FALSE -> il ciclo si ferma
